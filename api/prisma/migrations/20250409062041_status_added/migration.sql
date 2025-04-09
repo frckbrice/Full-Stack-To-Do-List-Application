4f8bbe8931146e24f7b350179ff9fd7fbfd6d7f1
frckbrice <maebrie2017@gmail.com>
2025-04-09 08:05:32 +0100

    feat(api): TODO CRUD done

---
+++ api/prisma/migrations/20250409062041_status_added/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Todo" ADD COLUMN     "status" TEXT;
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT ?? 5000;
+const dev_server_url = `${process.env.LOCAL_API_URL} `;
+const production_server_url = `${process.env.PROD_API_URL} `;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  const environment = process.env.NODE_ENV || 'development';
+  await app.listen(PORT, async () => {
+    console.log(
+      `Server running  in ${environment} mode on  ${environment === 'production' ? production_server_url : dev_server_url}`,
+    );
+  });
 }
 bootstrap();
